f167894581a31f5c4c264c55f9b14c01e85c8ad9
Liew Jia Wei <mjw.liew@gmail.com>
2024-04-14 20:13:15 +0800

    made changes

--- app/components/Intro.tsx
+++ app/components/Intro.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Intro = () => {
   return (
     <div className='m-14 p-5'>
-    <p className='text-6xl my-9 font-serif'>Hello0!</p>
+    <p className='text-6xl my-9 font-serif'>Hello!</p>
     <p className='text-2xl my-5 font-serif'>I&apos;m Liew Jia Wei.</p>
     <p className='text'>A current undergraduate studying Computer Science at the Nanyang Technology of Singapore. </p>
     <p className='text'>
--- app/layout.tsx
+++ app/layout.tsx
@@ -19,12 +19,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) { //can set theme here for html element (go to daisyui website to check)
   return (
-    <html lang="en" className = 'bg-gradient-to-r from-slate-300 to-white'> 
+    <html lang="en" > 
 
     <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
     </head>
-      <body className={inter.className}>{children}</body>
+      <body className='${inter.className} bg-gradient-to-r from-slate-300 to-white'>{children}</body>
     </html>
   )
 }
